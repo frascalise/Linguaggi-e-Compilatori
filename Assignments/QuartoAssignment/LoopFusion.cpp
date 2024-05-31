88e8820bfd7d6dd4569a7285627e0bb6b031c79a
frascalise <scalise92111@gmail.com>
2024-05-31 11:38:20 +0200

    Aggiunta divisione in funzioni e commenti al quarto assignment

--- Assignments/QuartoAssignment/LoopFusion.cpp
+++ Assignments/QuartoAssignment/LoopFusion.cpp
@@ -13,7 +13,9 @@
 
 using namespace llvm;
 
-bool guardedLoopCheck(Loop *loop, Loop *nextLoop) {
+/*  Ritorna true se i due loop guarded sono adiacenti, ovvero se tra i due loop
+    non sono presenti istruzioni. In caso contrario ritorna false */
+bool guardedAdjacentLoopCheck(Loop *loop, Loop *nextLoop) {
     /*  Nel preheader dovrebbe esserci il blocco di guardia
         Ricavo quindi il preheader, controllo che l'ultima istruzione sia un branch
         Se il branch è conditional (ovvero ha una condizione e in base a quella fa il salto)
@@ -24,13 +26,18 @@ bool guardedLoopCheck(Loop *loop, Loop *nextLoop) {
         return false;
 
     Instruction *term = preheader->getTerminator();
+
+    // Se non è un branch allora esci
     if (!term || !isa<BranchInst>(term)) 
         return false;
 
     BranchInst *branch = cast<BranchInst>(term);
+
+    // Un branch è incondizionale se non ha condizioni e salta sempre (es. while true)
     if (branch->isUnconditional()) 
         return false;
 
+    // Prendo le condizioni del branch in caso di true e false
     BasicBlock *trueSucc = branch->getSuccessor(0);
     BasicBlock *falseSucc = branch->getSuccessor(1);
 
@@ -40,82 +47,97 @@ bool guardedLoopCheck(Loop *loop, Loop *nextLoop) {
     return false;
 }
 
-bool notGuardedLoopCheck(Loop *loop, Loop *nextLoop){
+/*  Ritorna true se i due loop not guarded sono adiacenti, ovvero se tra i due loop
+    non sono presenti istruzioni. In caso contrario ritorna false */
+bool notGuardedAdjacentLoopCheck(Loop *loop, Loop *nextLoop){
     SmallVector<BasicBlock*> exitBB;
+    // Prendo tutti i blocchi di uscita del loop
     loop->getExitBlocks(exitBB);
 
+    // Scorro i BasicBlock di uscita del loop
     for(auto BB : exitBB){
+        // Se il BB è diverso dal preheader del prossimo loop allora ritorno false
         if(BB != nextLoop->getLoopPreheader())
             return false;
     }
+    // Se tutti i BB di uscita del loop sono uguali al preheader del prossimo loop allora ritorno true
     return true;
 }
+
+/*  Controllo che il numero di iterazioni dei due loop sia lo stesso
+    Ritorna true se i due loop iterano lo stesso numero di volte, altrimenti false */
+bool checkTripCount(Loop *L, Loop *nextL, ScalarEvolution &SE){
+    // Prendo il valore del numero di iterazioni che hanno i due loop
+    const SCEV *L0TripCount = SE.getBackedgeTakenCount(L);
+    const SCEV *L1TripCount = SE.getBackedgeTakenCount(nextL);
+
+    outs() << "Trip count del primo loop: ";
+    L0TripCount->print(outs());
+    outs() << "\nTrip count del secondo loop: ";
+    L1TripCount->print(outs());
+
+    if (L0TripCount == L1TripCount) {
+        outs() << "\tI loop iterano lo stesso numero di volte\n";
+        return true;
+    } else {
+        outs() << "\tI loop non iterano lo stesso numero di volte\n";
+        return false;
+    }
+}
+
+/*  Per controllare la dominanza tra due loop devo controllare che l'header del primo loop dominini il secondo loop
+    DT controlla la dominanza tra L0 (L) e L1 (nextL), ovvero che ogni percorso che arriva ad L1 passa per L0
+    PDT controlla la post dominanza tra L1 (nextL) e L0 (L), ovvero che se L1 viene eseguito vuol dire che L0 è già stato eseguito */
+bool checkLoopEquivalence(DominatorTree &DT, Loop *L, Loop *nextL, PostDominatorTree &PDT){
+    return DT.dominates(L->getHeader(), nextL->getHeader()) && PDT.dominates(nextL->getHeader(), L->getHeader());
+}
+
+
 
 PreservedAnalyses LoopFusion::run(Function &F,FunctionAnalysisManager &AM){
     LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
     DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
     PostDominatorTree &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
     ScalarEvolution &SE =AM.getResult<ScalarEvolutionAnalysis>(F);
+    
+    // Scorro i loop in ordine rovesciato, quindi parto dal primo anzichè partire dall'ultimo
     for(auto L = LI.rbegin(); L != LI.rend(); L++){
         
+        // nextL è il loop successivo a L
         auto nextL = L;
         nextL++;
 
-        if((*L)->isGuarded()){
-            outs()<<"Loop guarded\n";
-            if(nextL != LI.rend())
-                if(guardedLoopCheck(*L, *(nextL))){
-                    outs()<<"Loop adiacenti\n";
-                    //controllo equivalenza (punto 3)
-                    if (DT.dominates((*L)->getHeader(), (*nextL)->getHeader()) && PDT.dominates((*nextL)->getHeader(), (*L)->getHeader())) {
-                        outs() << "I loop sono equivalenti per flusso di controllo\n";
-
-                        //Controllo trip count (punto 2)
-                        const SCEV *L0TripCount = SE.getBackedgeTakenCount(*L);
-                        const SCEV *L1TripCount = SE.getBackedgeTakenCount(*nextL);
-                        outs() << "Trip count del primo loop: ";
-                        L0TripCount->print(outs());
-                        outs() << "\nTrip count del secondo loop: ";
-                        L1TripCount->print(outs());
-
-                        if (L0TripCount == L1TripCount) {
-                            outs() << "I loop iterano lo stesso numero di volte\n";
-                        } else {
-                            outs() << "I loop non iterano lo stesso numero di volte\n";
-                        }
-                    } else {
-                        outs() << "I loop non sono equivalenti per flusso di controllo\n";
-                    }
-                }
-        }
-        else{
-            outs()<<"Loop not guarded\n";
-            if(nextL != LI.rend())
-                if(notGuardedLoopCheck(*L, *(nextL))){
-                    outs()<<"Loop adiacenti\n";
-                    //controllo equivalenza (punto 3)
-                    if (DT.dominates((*L)->getHeader(), (*nextL)->getHeader()) && PDT.dominates((*nextL)->getHeader(), (*L)->getHeader())) {
-                        outs() << "I loop sono equivalenti per flusso di controllo\n";
-                        //Controllo trip count (punto 2)
-                        const SCEV *L0TripCount = SE.getBackedgeTakenCount(*L);
-                        const SCEV *L1TripCount = SE.getBackedgeTakenCount(*nextL);
-                        outs() << "Trip count del primo loop: ";
-                        L0TripCount->print(outs());
-                        outs() << "\nTrip count del secondo loop: ";
-                        L1TripCount->print(outs());
-
-                        if (L0TripCount == L1TripCount) {
-                            outs() << "I loop iterano lo stesso numero di volte\n";
-                        } else {
-                            outs() << "I loop non iterano lo stesso numero di volte\n";
-                        }
-                    } else {
-                        outs() << "I loop non sono equivalenti per flusso di controllo\n";
-                    }
-                }
-
+        // Controllo se il loop che stiamo scorrendo è l'ultimo
+        if(nextL == LI.rend()){
+            continue;
         }
         
+        /*
+            - Loop GUARDED: se il loop è protetto da una guardia, ovvero se il loop è preceduto da un branch (while x < 5)
+            - Loop NOT GUARDED: se il loop non è protetto da una guardia (while true)
+        */
+        if((*L)->isGuarded()) {
+            outs()<<"Loop guarded\n";
+            if(!guardedAdjacentLoopCheck(*L, *(nextL)))
+                continue;
+        } else {
+            outs()<<"Loop not guarded\n";
+            if(!notGuardedAdjacentLoopCheck(*L, *(nextL)))
+                continue;
+        }
+
+        // Controllo equivalenza (punto 3)
+        // Controllo per Dominanza e Post-Dominanza
+        if (!checkLoopEquivalence(DT, *L, *nextL, PDT)) {
+            continue;
+        }
+
+        // Controllo trip count (punto 2)
+        // Controllo che il numero di iterazioni dei due loop sia lo stesso
+        if(!checkTripCount(*L, *nextL, SE)){
+            continue;
+        }
+/*
         outs()<<"PREHEADER: "<<*((*L)->getLoopPreheader())<<"\n";
         outs()<<"HEADER: "<<*((*L)->getHeader())<<"\n";
         outs()<<"CONTENUTO\n";
@@ -124,8 +146,8 @@ PreservedAnalyses LoopFusion::run(Function &F,FunctionAnalysisManager &AM){
         }
         outs()<<"EXITING BLOCK: "<<*((*L)->getExitingBlock())<<"\n";
         outs()<<"EXIT BLOCK: "<<*((*L)->getExitBlock())<<"\n";
-        
-        //outs()<<"successore di exiting block"<<(*L)->getSingleSuccessor()<<'\n';
+        outs()<<"successore di exiting block"<<(*L)->getSingleSuccessor()<<'\n';
+*/        
     }
     return PreservedAnalyses::all();
 }
