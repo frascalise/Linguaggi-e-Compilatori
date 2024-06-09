c864fb7f627648b19e133977bd10e3bd30577984
Salvatore Di Lorenzo <32141810+salor02@users.noreply.github.com>
2024-06-09 18:50:55 +0200

    completo

--- Assignments/QuartoAssignment/LoopFusion.cpp
+++ Assignments/QuartoAssignment/LoopFusion.cpp
@@ -1,4 +1,4 @@
-#include "llvm/Transforms/Utils/LFusion.h"
+#include "llvm/Transforms/Utils/LoopFusion.h"
 #include "llvm/IR/Instructions.h"
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/Transforms/Utils/LoopRotationUtils.h"
@@ -9,6 +9,7 @@
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/ADT/GenericCycleImpl.h"
 #include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/DependenceAnalysis.h"
 #include <llvm/IR/Constants.h>
 
 #include <vector>
@@ -94,22 +95,45 @@ bool checkLoopEquivalence(DominatorTree &DT, Loop *L, Loop *nextL, PostDominator
     return DT.dominates(L->getHeader(), nextL->getHeader()) && PDT.dominates(nextL->getHeader(), L->getHeader());
 }
 
+/*  Non funzionante */
+bool negativeDependence(Loop *L, Loop *nextL, DependenceInfo &DI){
+
+    for(auto BB_L : L->getBlocks()){
+        for(auto BB_nextL : nextL->getBlocks()){
+            for(auto instr_L = BB_L->begin(); instr_L != BB_L->end(); instr_L++){
+                for(auto instr_nextL = BB_nextL->begin(); instr_nextL != BB_nextL->end(); instr_nextL++){
+                    //outs()<<"ciao\n";
+                    Instruction* src = dyn_cast<Instruction>(instr_L);
+                    Instruction* dst = dyn_cast<Instruction>(instr_nextL);
+                    auto dep = DI.depends(src, dst, true);
+
+                    if(dep){
+                        outs()<<"src: "<<*(dep->getSrc())<<'\n';
+                        outs()<<"dst: "<<*(dep->getDst())<<'\n';
+                        unsigned int dir = dep->getDirection(dep->getLevels());
+                        outs()<<dir<<'\n';
+                        /*
+                        const SCEV *distance = dep->getDistance(dep->getLevels());
+                        if(distance){
+                            outs()<<"ciao\n";
+                            //distance->print(outs());
+                        }*/
+                        outs()<<'\n';
+                    }
+                }
+            }
+        }
+    }
+    return true;
+}
+
 /*  Linka il blocco src al blocco dst modificando l'operatore del branch incondizionato del terminatore
     del blocco src e facendolo puntare al blocco dst
     NB: questo metodo funziona solo nel caso in cui alla fine del blocco ci sia un branch terminator    */
 void linkBlocks(BasicBlock* src, BasicBlock* dst, unsigned int idx){
-    //outs() << "\n[ SRC ]" << *src;
-    //outs() << "\n[ DST ]" << *dst;
-
     Instruction* terminator = src->getTerminator();
-    //outs() << "[ TERMINATOR ]\n" << *terminator << "\n";
-    
     BranchInst *branch = cast<BranchInst>(terminator);
-    //outs() << "[ BRANCH ]\n" << *branch << "\n";
-    
     branch->setSuccessor(idx, dst);
-    //outs() << "[ BRANCH MODIFICATO ]\n" << *branch << "\n";
-    //outs() << "[ SRC MODIFICATO ]" << *src;
 }
 
 /*  Trova tutti i blocchi che compongono il body del loop. Questa lista è ottenuta come differenza dell'insieme dei
@@ -135,48 +159,41 @@ void getBodyBlocks(Loop *L, std::vector<BasicBlock*>& bodyBlocks){
             continue; 
         bodyBlocks.push_back(BB);  
     }
-
 }
 
-/*  Sostituisce tutti gli usi dell'incrementatore del loop B (nextL) con l'incrementatore del loop A (L). Gli incrementatori
+/*  Sostituisce tutti gli usi dell'incrementatore del loop B con l'incrementatore del loop A. Gli incrementatori
     si trovano nei phi node all'interno degli header dei loop e, quindi, appena viene trovato un phi si salva il valore */
 void switchIncr(BasicBlock* headerL, BasicBlock* headerNextL){
-    /*  Selezione riferimento all'incrementatore del loop A (L). Occorre prendere questo riferimento
-        per sostituirilo a tutti gli usi dell'incrementatore del loop B (nextL) all'interno del body del loop
-        B (nextL). */
+
+    /*  Selezione riferimento all'incrementatore del loop A. Occorre prendere questo riferimento
+        per sostituirilo a tutti gli usi dell'incrementatore del loop B all'interno del body del loop
+        B. */
     PHINode* incrL = NULL;
     for(auto instr = headerL->begin(); instr != headerL->end(); instr++){
-        //outs() << "\n[ INCREMENTATORE di L ]\n" << *instr << "\n";
         if((incrL = dyn_cast<PHINode>(instr)))
             break;
     }
 
-    /*  Selezione riferimento a incrementatore del loop B (nextL) (da sostituire con riferimento a 
-        incrementatore del loop A (L) ) */
+    /*  Selezione riferimento a incrementatore del loop B (da sostituire con riferimento a 
+        incrementatore del loop A) */
     PHINode* incrNextL = NULL;
     for(auto instr = headerNextL->begin(); instr != headerNextL->end(); instr++){
-        //outs() << "\n[ INCREMENTATORE di nextL ]\n" << *instr << "\n";
         if((incrNextL = dyn_cast<PHINode>(instr)))
             break;
     }
 
-    //sostituzione incrementatore del loop B (nextL)
+    //sostituzione incrementatore del loop B
     incrNextL->replaceAllUsesWith(incrL);
-    
-    /* [ DEBUG: SOSTITUZIONE INCREMENTATORE] */
-    /*outs() << "\n[ DEBUG: SOSTITUZIONE INCREMENTATORE ]\n";
-    outs() << "[ INCR. di L ] \n"<<*incrL << "\n";
-    outs() << "\n[ INCR. di nextL ] \n"<<*incrNextL << "\n";*/
 }
 
 /*  Questa funzione si occupa di unire i due loop in questione, in ordine:
-    1. sostituisce tutti gli usi dell'incrementatore del loop B (nextL) con l'incrementatore del loop A (L)
-    2. sposta tutti i blocchi del body del loop B prima del latch del loop A (L)
-    3. vari link dei blocchi per completare l'unione e isolare il vecchio CFG del loop B (nextL) per poter eseguire
+    1. sostituisce tutti gli usi dell'incrementatore del loop B con l'incrementatore del loop A
+    2. sposta tutti i blocchi del body del loop B prima del latch del loop A
+    3. vari link dei blocchi per completare l'unione e isolare il vecchio CFG del loop B per poter eseguire
        una dead code elimination successivamente */
 void mergeLoops(Loop *L, Loop *nextL){
     
-    /*  Salvataggio dei blocchi principali del loop B (nextL) perchè una volta modificata la sua struttura non
+    /*  Salvataggio dei blocchi principali del loop B perchè una volta modificata la sua struttura non
         saranno più accessibili.
         NB: questo passo funziona solo se i loop in question hanno un solo latch e un solo exit */
     BasicBlock* exitNextL = nextL->getExitBlock();
@@ -189,17 +206,6 @@ void mergeLoops(Loop *L, Loop *nextL){
     std::vector<BasicBlock*> bodyL;
     getBodyBlocks(L, bodyL);
 
-    /* [ DEBUG: BLOCCHI PRINCIPALI LOOP nextL ] */
-    /*outs() << "\n==========[ DEBUG: BLOCCHI PRINCIPALI nextL ]==========\n";
-    outs() << "\n[ Header ]" << *headerNextL;
-    outs() << "\n[ Body ]";
-    for(auto BB : bodyNextL)
-        outs() << *BB;
-    outs() << "\n[ Latch ]" << *latchNextL;
-    outs() << "\n[ Exit ]" << *exitNextL;
-    outs() << "=========================================================\n";*/
-
-
     //sostituzione incrementatori
     switchIncr(L->getHeader(), headerNextL);
 
@@ -210,39 +216,28 @@ void mergeLoops(Loop *L, Loop *nextL){
         BB->moveBefore(latchL);
     }
 
-    /*  Linking dell'ultimo blocco del body del loop B (nextL) al latch del loop A (L)  */
+    /*  Linking dell'ultimo blocco del body del loop B al latch del loop A  */
     linkBlocks(bodyNextL.back(), latchL, 0);
     
-    /*  Linking dell'ultimo blocco del body del loop A (L) al primo blocco del body del loop B (nextL) */
+    /*  Linking dell'ultimo blocco del body del loop A al primo blocco del body del loop B */
     linkBlocks(bodyL.back(), bodyNextL.front(), 0);
 
-    /*  Linking dell'header del loop A (L) al blocco d'uscita del loop B (nextL) (nel ramo false)*/
+    /*  Linking dell'header del loop A al blocco d'uscita del loop B (nel ramo false)*/
     linkBlocks(L->getHeader(), exitNextL, 1);
 
     /*  Linking dell'header del loop B al latch del loop B (sia true che false). In questo modo il codice del loop
         B sarà completamente isolato e potrà essere eliminato con il passo "simplifycfg".*/
     linkBlocks(headerNextL, latchNextL, 0);
     linkBlocks(headerNextL, latchNextL, 1);
-
-    /* [ DEBUG: PRINT LOOP L ] */
-    /*outs() << "\n==========[ DEBUG: PRINT L ]==========\n";
-    outs() << "\n[ Header ]" << *L->getHeader();
-    outs() << "\n[ Body ]";
-    for(auto BB : bodyL)    
-        outs() << *BB;
-    for(auto BB : bodyNextL)
-        outs() << *BB;
-    outs() << "\n[ Latch ]" << *L->getLoopLatch();
-    outs() << "========================================\n";*/
-
 }
 
 
-PreservedAnalyses LFusion::run(Function &F,FunctionAnalysisManager &AM){
+PreservedAnalyses LoopFusion::run(Function &F,FunctionAnalysisManager &AM){
     LoopInfo &LI = AM.getResult<LoopAnalysis>(F);
     DominatorTree &DT = AM.getResult<DominatorTreeAnalysis>(F);
     PostDominatorTree &PDT = AM.getResult<PostDominatorTreeAnalysis>(F);
     ScalarEvolution &SE =AM.getResult<ScalarEvolutionAnalysis>(F);
+    DependenceInfo &DI = AM.getResult<DependenceAnalysis>(F);
     
     // Scorro i loop in ordine rovesciato, quindi parto dal primo anzichè partire dall'ultimo
     for(auto L = LI.rbegin(); L != LI.rend(); L++){
@@ -281,6 +276,11 @@ PreservedAnalyses LFusion::run(Function &F,FunctionAnalysisManager &AM){
         if(!checkTripCount(*L, *nextL, SE)){
             continue;
         }
+
+        /*
+        if(!negativeDependence(*L, *nextL, DI)){
+            continue;
+        }*/
 
         mergeLoops(*L, *nextL);
     
